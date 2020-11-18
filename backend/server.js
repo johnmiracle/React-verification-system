c8d6fd14861e8da90fea1817cb62e4457e826e15
miracle <anajemiracle@gmail.com>
2020-11-18 10:29:17 +0100

    third commit

--- backend/server.js
+++ backend/server.js
@@ -40,9 +40,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use(express.static(path.join(__dirname, '/client/build')));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/frontend/build/index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/client/build/index.html')));
 
 app.use(function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
